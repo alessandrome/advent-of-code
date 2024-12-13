40e812db92e775ba6f6cebdc7bf77339a3284809
Alessandro <mesti.alessandro.git@gmail.com>
2024-12-13 19:25:10 +0100

    Using stupid brutal method for part 2 of day 2 (2024)

--- 2024/2/day_2.py
+++ 2024/2/day_2.py
@@ -2,6 +2,17 @@ import math
 import sys
 
 
+def calculate_diff(levels):
+    return [levels[i + 1] - levels[i] for i in range(len(levels) - 1)]
+
+def substitute_diff(levels_diff, index):
+    if index == 0:
+        return levels_diff[index + 1:]
+    if index == len(levels_diff) - 1:
+        return levels_diff[:index]
+    return levels_diff[:index] + [levels_diff[index] + levels_diff[index + 1]] + levels_diff[index + 2:]
+
+
 def check_difference_safeness(levels_diff):
     is_increasing = None
     for i in range(len(levels_diff)):
@@ -15,8 +26,38 @@ def check_difference_safeness(levels_diff):
                 return i
     return -1
 
+def stupid_check_safeness_with_dampener(levels):
+    def generate_dampened_sublist(_levels: list):
+        _sub_levels = [_levels]
+        for i in range(len(_levels)):
+            level = _levels.copy()
+            level.pop(i)
+            _sub_levels.append(level)
+        return _sub_levels
+
+    found = False
+    sub_levels = generate_dampened_sublist(levels)
+    for level in sub_levels:
+        print(level)
+        if check_difference_safeness(calculate_diff(level)) == -1:
+            found = True
+            break
+    print(f'------ {found} -----')
+    return found
 
 def check_safeness_with_dampener(levels_diff):
+    positive, negative, zeros, problems_i = [], [], [], []
+    for i in range(len(levels_diff)):
+        diff = levels_diff[i]
+        if diff > 0:
+            positive.append(i)
+            problems_i.append(i)
+        elif diff < 0:
+            negative.append(i)
+            problems_i.append(i)
+        else:
+            zeros.append(i)
+            problems_i.append(i)
     dampened = False
     safe = check_difference_safeness(levels_diff)
     if safe != -1:
@@ -43,17 +84,34 @@ if __name__ == "__main__":
     values_safeness = []
     values_safeness_with_dampener = []
     try:
+        safe_values = []
+        unsafe_values = []
+        dampened_safe_values = []
         with open(file_path, 'r') as file:
             for line in file:
                 last_values = [int(el) for el in line.split(' ')]
                 values.append(last_values)
                 last_differences = [last_values[i+1] - last_values[i] for i in range(len(last_values) - 1)]
                 values_differences.append(last_differences)
-                values_safeness.append(1 if check_difference_safeness(last_differences) == -1 else 0)
-                values_safeness_with_dampener.append(1 if check_safeness_with_dampener(last_differences)[0] else 0)
+
+                if check_difference_safeness(last_differences) == -1:
+                    safe_values.append(last_values)
+                    values_safeness.append(1)
+                else:
+                    if stupid_check_safeness_with_dampener(last_values):
+                        dampened_safe_values.append(last_values)
+                    else:
+                        unsafe_values.append(last_values)
+                # values_safeness.append(1 if check_difference_safeness(last_differences) == -1 else 0)
+                # values_safeness_with_dampener.append(1 if check_safeness_with_dampener(last_differences)[0] else 0)
+                values_safeness_with_dampener.append(1 if stupid_check_safeness_with_dampener(last_differences) else 0)
         print(f"Number of elements: {len(values)}")
-        print(f"Number of safe levels: {sum(values_safeness)}")  # Request for Day 2 - Part 1 problem
-        print(f"Number of safe dampered levels: {sum(values_safeness_with_dampener)}")  # Request for Day 2 - Part 2 problem
+        # print(f"Number of safe levels: {sum(values_safeness)}")  # Request for Day 2 - Part 1 problem
+        # print(f"Number of safe dampered levels: {sum(values_safeness_with_dampener)}")  # Request for Day 2 - Part 2 problem
+        print(f"Number of safe levels: {len(safe_values)}")  # Request for Day 2 - Part 1 problem
+        print(f"Number of unsafe levels: {len(unsafe_values)}")  # Request for Day 2 - Part 2 problem
+        print(f"Number of dampened safe levels: {len(dampened_safe_values)}")  # Request for Day 2 - Part 2 problem
+        print(f"{[71, 69, 70, 69, 66]} - Safe: {stupid_check_safeness_with_dampener(calculate_diff([71, 69, 70, 69, 66]))}")
     except FileNotFoundError:
         print(f"Error: File '{file_path}' not found.")
         sys.exit(2)
