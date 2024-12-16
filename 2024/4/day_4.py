ebc39ea4ebb7a0a6d533f295fbeba2da57f62079
Alessandro <mesti.alessandro.git@gmail.com>
2024-12-16 16:38:46 +0100

    Working on Day 4 2024 - Part 2 OK

--- 2024/4/day_4.py
+++ 2024/4/day_4.py
@@ -3,7 +3,8 @@ import re
 import sys
 
 # xmas = ['X', 'M', 'A', 'S']
-xmas = 'XMAS'
+xmas = 'XMAS'  # Part 1
+x_mas = 'MAS'  # Part 2
 xmas_reverse = xmas[::-1]
 
 HORIZONTAL = 0
@@ -43,6 +44,15 @@ def get_text(table, row, col, direction):
                 table[coords[3][0]][coords[3][1]])
     return coords, to_check
 
+def get_text_x_mas(table, row, col, direction):
+    coords = get_coords(row, col, direction)
+    to_check = (table[coords[0][0]][coords[0][1]] +
+                table[coords[1][0]][coords[1][1]] +
+                table[coords[2][0]][coords[2][1]])
+    return coords, to_check
+
+
+# Part 1 - XMAS Counter
 def xmas_counter(table):
     xmas_coords = []
     width = len(table[0])
@@ -94,6 +104,24 @@ def xmas_counter(table):
     return xmas_coords
 
 
+# Part 2 - X-MAS counter
+def x_mas_counter(table):
+    xmas_coords = []
+    width = len(table[0])
+    height = len(table)
+    for row in range(1, height - 1):
+        for col in range(1, width - 1):
+            if table[row][col] == x_mas[1]:
+                coords_br, to_check_br = get_text_x_mas(table, row - 1, col - 1, BOTTOM_RIGHT)
+                coords_tr, to_check_tr = get_text_x_mas(table, row + 1, col - 1, TOP_RIGHT)
+                to_check_tr_rev = to_check_tr[::-1]
+                to_check_br_rev = to_check_br[::-1]
+                if (to_check_tr == x_mas or to_check_tr_rev == x_mas) and (to_check_br == x_mas or to_check_br_rev == x_mas):
+                    if to_check_tr == to_check_br or to_check_tr == to_check_br_rev or to_check_tr_rev == to_check_br or to_check_tr_rev == to_check_br_rev:
+                        xmas_coords.append((coords_tr, coords_br))
+    return xmas_coords
+
+
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: python day_4.py <file_path>")
@@ -103,9 +131,9 @@ if __name__ == "__main__":
     try:
         with open(file_path, 'r') as file:
             for line in file:
-                input_string.append([*line])
-        print(len(xmas_counter(input_string)))
-        print(xmas == ['X', 'M', 'A', 'S'])
+                input_string.append([*line[:len(line) - 1]])
+        print(f"XMAS Count: {len(xmas_counter(input_string))}")
+        print(f"X-MAS Count: {len(x_mas_counter(input_string))}")
     except FileNotFoundError:
         print(f"Error: File '{file_path}' not found.")
         sys.exit(2)
